c06acab49810db185e02d9a9027b47e734ee1c3e
Matthew Wigginton Bhagat-Conway <matt@indicatrix.org>
2022-06-17 21:31:29 -0400

    log variables

--- analysis.R
+++ analysis.R
@@ -1,8 +1,8 @@
 data(mtcars)
 
 # How is mpg related to displacement
-res.fit = lm(mpg~disp+cyl, mtcars)
+res.fit = lm(log(mpg)~disp+cyl, mtcars)
 summary(res.fit)
 
-res.fit2 = lm(mpg~disp+cyl+wt, mtcars)
+res.fit2 = lm(log(mpg)~disp+cyl+wt, mtcars)
 ssummary(res.fit2)
